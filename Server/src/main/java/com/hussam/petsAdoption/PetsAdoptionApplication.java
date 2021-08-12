91f0ff4d7fa31e79c9e671af305115542ceb1dc7
hussam asfour <hussam_asfour@yahoo.com>
2021-08-12 11:23:56 -0400

    Initial commit

---
+++ Server/src/main/java/com/hussam/petsAdoption/PetsAdoptionApplication.java
@@ -0,0 +1,13 @@
+package com.hussam.petsAdoption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsAdoptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsAdoptionApplication.class, args);
+	}
+
+}
---
+++ Server/src/test/java/com/hussam/petsAdoption/PetsAdoptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hussam.petsAdoption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsAdoptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
