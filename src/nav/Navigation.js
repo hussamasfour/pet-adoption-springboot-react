866e1bd61d60b3da3e47f42a5fbdbc29d2c0fb64
hussam asfour <hussam_asfour@yahoo.com>
2021-08-20 17:08:56 -0400

    fixed toggle button in nav

--- src/nav/Navigation.js
+++ src/nav/Navigation.js
@@ -10,6 +10,17 @@ const Navigation = () => {
             PETOS
           </Link>
         </div>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
         <div
           className="collapse navbar-collapse justify-content-end"
@@ -21,13 +32,13 @@ const Navigation = () => {
                 Home
               </Link>
             </li>
-            <li className="nav-item ">
+            <li className="nav-item mx-3 ">
               <Link className="nav-link text-white" to="/explore">
                 Explore
               </Link>
             </li>
 
-            <li className="nav-item">
+            <li className="nav-item mx-3">
               <Link className="nav-link text-white" to="/login">
                 Login
               </Link>
